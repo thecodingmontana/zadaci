0b8a35b30422c1b1fc4759a4f25906de44b8f42b
thecodingmontana <dev.humblechris254@gmail.com>
2026-07-04 08:23:06 +0300

    chore: add commitlint config for conventional commit validation

---
+++ commitlint.config.ts
@@ -0,0 +1,31 @@
+export default {
+  extends: [],
+  rules: {
+    "body-leading-blank": [2, "always"],
+    "footer-leading-blank": [2, "always"],
+    "header-max-length": [2, "always", 72],
+    "scope-case": [2, "always", "lower-case"],
+    "subject-case": [2, "never", ["upper-case", "start-case", "pascal-case"]],
+    "subject-empty": [2, "never"],
+    "subject-full-stop": [2, "never", "."],
+    "type-case": [2, "always", "lower-case"],
+    "type-empty": [2, "never"],
+    "type-enum": [
+      2,
+      "always",
+      [
+        "build",
+        "chore",
+        "ci",
+        "docs",
+        "feat",
+        "fix",
+        "perf",
+        "refactor",
+        "revert",
+        "style",
+        "test",
+      ],
+    ],
+  },
+};
