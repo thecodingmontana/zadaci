538c96ff39c635d2c6ad43e8aa857bbbde190a12
thecodingmontana <dev.humblechris254@gmail.com>
2026-07-09 20:24:38 +0300

    chore: added ebra's name to the list

--- app/components/hero-section.vue
+++ app/components/hero-section.vue
@@ -20,6 +20,7 @@ import { MultiplayerText } from "./ui/multiplayer-text";
           { name: 'Olive Bishop', color: '#3ecf8e' },
           { name: 'Blackie', color: '#f0883e' },
           { name: 'thecodingmontana', color: '#22d3ee' },
+          { name: 'Ebra Kitavey', color: '#ffb703' },
         ]"
         :interval="1500"
       />
